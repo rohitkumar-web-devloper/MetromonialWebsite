e0f8b8f58f1ce391131784c2329f32133b17e154
TusharGola28 <golat860@gmail.com>
2025-01-10 15:21:05 +0530

    add

---
+++ components/AdDetailsModal.tsx
@@ -0,0 +1,222 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog'
+import {
+  BadgeCheck,
+  Blend,
+  Phone,
+  PhoneCall,
+  Search,
+  SlidersHorizontal,
+  User
+} from 'lucide-react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import { Autoplay, Navigation } from 'swiper/modules'
+import ImageDisplay from './ImageDisplay'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from './ui/tooltip'
+const AdsDetailsModal = ({ open, close, selectedData }) => {
+
+  return (
+    <div>
+      <Dialog open={open} onOpenChange={close}>
+        <DialogContent className='border-2 border-white border-opacity-20 sm:max-w-[700px]'>
+          <DialogHeader>
+            <DialogTitle className='flex items-center gap-2'>
+              Details
+            </DialogTitle>
+          </DialogHeader>
+          <div className='py-4 clip-scroll max-h-[400px] overflow-y-scroll'>
+            <div>
+              <Swiper
+                breakpoints={{
+                  0: {
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                  }
+                }}
+                navigation={true}
+                modules={[Navigation, Autoplay]}
+                autoplay={{
+                  delay: 5000,
+                  disableOnInteraction: false
+                }}
+                className='mySwiper'
+              >
+                {JSON.parse(selectedData?.profile).map((it, index) => {
+                  return (
+                    <SwiperSlide key={index} className=''>
+                      <div className='relative'>
+                        <ImageDisplay imageUrl={it} xs='200px' />
+                        <div className='top-2 left-0 absolute bg-primary px-2 py-1 rounded-r-full'>
+                          <p className='flex items-center gap-1 text-[12px]'>
+                            <BadgeCheck size={20} />
+                            Premium
+                          </p>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  )
+                })}
+              </Swiper>
+            </div>
+            <div>
+              <div className='py-4 p-2'>
+                <h1 className='text-white'>{selectedData?.title}</h1>
+                <h1 className='mt-1 text-[12px] text-ellipsis text-white whitespace-nowrap overflow-hidden'>
+                  {selectedData?.description}
+                </h1>
+
+                <div className='flex gap-2 mt-2'>
+                  <div className='flex items-center gap-2 bg-[#d4d4d41a] mt-2 px-2 py-1 rounded-xl text-[12px] text-white'>
+                    <Blend size={15} /> {selectedData?.nationality}
+                  </div>
+                  <div className='flex items-center gap-1 bg-[#d4d4d41a] mt-2 px-2 py-1 rounded-xl text-[12px] text-white'>
+                    <User size={15} /> {selectedData?.age}
+                  </div>
+                </div>
+                <h1 className='mt-2 text-primary'>Attention to</h1>
+                <div className='flex gap-2 mt-1'>
+                  {selectedData?.attentionTo.map(item => {
+                    return (
+                      <div
+                        key={item?.name}
+                        className='flex items-center gap-2 bg-[#d4d4d41a] mt-2 px-2 py-1 rounded-xl text-[12px] text-white'
+                      >
+                        <Blend size={15} /> {item?.name}
+                      </div>
+                    )
+                  })}
+                </div>
+                <h1 className='mt-2 text-primary'>Body Type</h1>
+                <div className='flex gap-2 mt-1'>
+                  <div className='flex items-center gap-2 bg-[#d4d4d41a] mt-2 px-2 py-1 rounded-xl text-[12px] text-white'>
+                    <Blend size={15} /> {selectedData?.bodyType}
+                  </div>
+                </div>
+                <h1 className='mt-2 text-primary'>Breast</h1>
+                <div className='flex gap-2 mt-1'>
+                  <div className='flex items-center gap-2 bg-[#d4d4d41a] mt-2 px-2 py-1 rounded-xl text-[12px] text-white'>
+                    <Blend size={15} /> {selectedData?.breast}
+                  </div>
+                </div>
+                <h1 className='mt-2 text-primary'>Hair</h1>
+                <div className='flex gap-2 mt-1'>
+                  <div className='flex items-center gap-2 bg-[#d4d4d41a] mt-2 px-2 py-1 rounded-xl text-[12px] text-white'>
+                    <Blend size={15} /> {selectedData?.hair}
+                  </div>
+                </div>
+                <h1 className='mt-2 text-primary'>Place of Services</h1>
+                <div className='flex gap-2 mt-1'>
+                  {selectedData?.placeOfServices.map(it => {
+                    return (
+                      <div
+                        key={it?.name}
+                        className='flex items-center gap-2 bg-[#d4d4d41a] mt-2 px-2 py-1 rounded-xl text-[12px] text-white'
+                      >
+                        <Blend size={15} /> {it?.name}
+                      </div>
+                    )
+                  })}
+                </div>
+                <h1 className='mt-2 text-primary'>Services</h1>
+                <div className='flex flex-wrap gap-2 mt-1'>
+                  {selectedData?.services.map(it => {
+                    return (
+                      <div
+                        key={it?.name}
+                        className='flex items-center gap-2 bg-[#d4d4d41a] mt-2 px-2 py-1 rounded-xl text-[12px] text-white'
+                      >
+                        <Blend size={15} /> {it?.name}
+                      </div>
+                    )
+                  })}
+                </div>
+                <h1 className='mt-2 text-primary'>Payment Mode</h1>
+                <div className='flex gap-2 mt-1'>
+                  {JSON.parse(selectedData?.paymentMethod[0]).map(it => {
+                    return (
+                      <div
+                        key={it}
+                        className='flex items-center gap-2 bg-[#d4d4d41a] mt-2 px-2 py-1 rounded-xl text-[12px] text-white'
+                      >
+                        <Blend size={15} /> {it}
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+          <DialogFooter>
+            <div className='flex justify-between items-center w-full'>
+              <div className='flex items-center gap-2'>
+                <div className='bg-[#d4d4d41a] rounded-xl'>
+                  <h1 className='px-3 py-2 text-white'>
+                    ₹ {selectedData?.pricePerHour} Per hour
+                  </h1>
+                </div>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <div
+                        className='flex items-center gap-1 bg-[#d4d4d41a] p-2 rounded-xl text-white cursor-pointer'
+                        onClick={() =>
+                          window.open(`tel:${selectedData?.mobileNumber}`)
+                        }
+                      >
+                        <Phone size={20} />
+                      </div>
+                    </TooltipTrigger>
+                    <TooltipContent className='bg-transparent'>
+                      <p className=''>{selectedData?.mobileNumber}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <div
+                        className='flex items-center gap-1 bg-[#d4d4d41a] p-2 rounded-xl text-white cursor-pointer'
+                        onClick={() =>
+                          window.open(
+                            `https://wa.me/${
+                              selectedData?.whatsAppNumber
+                            }?text=${encodeURIComponent('Hello')}`
+                          )
+                        }
+                      >
+                        <PhoneCall size={20} />
+                      </div>
+                    </TooltipTrigger>
+                    <TooltipContent className='bg-transparent'>
+                      <p className=''>{selectedData?.whatsAppNumber}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </div>
+              <Button type='button' className='font-semibold' onClick={close}>
+                Close
+              </Button>
+            </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
+}
+
+export { AdsDetailsModal }
--- views/posts/post-page.tsx
+++ views/posts/post-page.tsx
@@ -27,9 +27,17 @@ import { Skeleton } from '@/components/ui/skeleton'
 import Image from 'next/image'
 import No_Data from '../../assets/no_feedback-67e33c89.svg'
 import { GlobalSearchModal } from '@/components/GlobalSearch'
+import { AdsDetailsModal } from '@/components/AdDetailsModal'
+import { useModalControl } from '@/hooks'
 const PostPage = ({ searchParams }: PostPageType) => {
   const router = useRouter()
   const [open, setOpen] = useState(false)
+  const [selectedData, setSelectedData] = useState()
+  const {
+    open: isAddOpen,
+    handleCloseModal,
+    handleOpenModal
+  } = useModalControl()
   const filters = {
     categoryId: +searchParams?.id,
     attentionTo: searchParams?.attentionTo
@@ -52,7 +60,7 @@ const PostPage = ({ searchParams }: PostPageType) => {
     state: searchParams?.state ? searchParams?.state : undefined
   }
   const { data, loading } = useQuery(get_premium_ads, {
-    variables: { page: 1, pageSize: 12, filter: filters },
+    variables: { page: 1, pageSize: 12, filter: filters }
     // skip: !!!searchParams?.id
   })
   return (
@@ -71,19 +79,17 @@ const PostPage = ({ searchParams }: PostPageType) => {
       </div>
       {data?.premiumAds?.ads.length > 0 && (
         <div className='flex justify-between items-center mt-2'>
-           <h1 className='flex items-center gap-2 mt-4 text-2xl text-primary'>
+          <h1 className='flex items-center gap-2 mt-4 text-2xl text-primary'>
             <ArrowLeft
               className='cursor-pointer'
               onClick={() => router.back()}
             />
-               Toppremium {searchParams?.name}
+            Toppremium {searchParams?.name}
           </h1>
           <Button
             variant='link'
             onClick={() =>
-              router.push(
-                `/posts/premium?data=${JSON.stringify(searchParams)}`
-              )
+              router.push(`/posts/premium?data=${JSON.stringify(searchParams)}`)
             }
           >
             View all
@@ -91,8 +97,8 @@ const PostPage = ({ searchParams }: PostPageType) => {
         </div>
       )}
       {loading && (
-        <div className='gap-4 grid grid-cols-4 mt-6'>
-          {[...Array(4)].map((_, index) => {
+        <div className='gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-6'>
+          {[...Array(8)].map((_, index) => {
             return (
               <div className='flex flex-col space-y-3' key={index}>
                 <Skeleton className='rounded-xl h-[200px]' />
@@ -137,43 +143,38 @@ const PostPage = ({ searchParams }: PostPageType) => {
               }
             }}
             navigation={true}
-            modules={[Navigation]}
-            // autoplay={{
-            //   delay: 10000,
-            //   disableOnInteraction: false
-            // }}
+            modules={[Navigation, Autoplay]}
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: false
+            }}
             className='mySwiper'
           >
             {data &&
               data?.premiumAds?.ads?.map(item => {
                 return (
-                  <SwiperSlide key={item?.id} className=''>
+                  <SwiperSlide
+                    key={item?.id}
+                    className=''
+                    onClick={() => {
+                      handleOpenModal()
+                      setSelectedData(item)
+                    }}
+                  >
                     <div className='border-2 border-white bg-[#d4d4d41a] border-opacity-15 rounded-xl cursor-pointer'>
-                      <Swiper
-                        slidesPerView={1}
-                        // pagination={{
-                        //   clickable: true
-                        // }}
-                        // autoplay={{
-                        //   delay: 2500,
-                        //   disableOnInteraction: false
-                        // }}
-                        // modules={[Autoplay]}
-                      >
-                        {JSON.parse(item?.profile).map(it => {
-                          return (
-                            <SwiperSlide key={it} className='relative'>
-                              <ImageDisplay imageUrl={it} xs='200px' />
-                              <div className='top-2 left-0 absolute bg-primary px-2 py-1 rounded-r-full'>
-                                <p className='flex items-center gap-1 text-[12px]'>
-                                  <BadgeCheck size={20} />
-                                  Premium
-                                </p>
-                              </div>
-                            </SwiperSlide>
-                          )
-                        })}
-                      </Swiper>
+                      {[JSON.parse(item?.profile)[0]].map(it => {
+                        return (
+                          <div key={it} className='relative'>
+                            <ImageDisplay imageUrl={it} xs='200px' />
+                            <div className='top-2 left-0 absolute bg-primary px-2 py-1 rounded-r-full'>
+                              <p className='flex items-center gap-1 text-[12px]'>
+                                <BadgeCheck size={20} />
+                                Premium
+                              </p>
+                            </div>
+                          </div>
+                        )
+                      })}
 
                       <div className='py-4 p-2'>
                         <h1 className='text-white'>{item?.title}</h1>
@@ -202,7 +203,89 @@ const PostPage = ({ searchParams }: PostPageType) => {
           </Swiper>
         </div>
       )}
-      {open && <GlobalSearchModal open={open} close={() => setOpen(false)} searchParams={searchParams} />}
+      {data?.premiumAds?.ads.length > 0 && (
+        <div className='flex justify-between items-center mt-2'>
+          <h1 className='flex items-center gap-2 mt-4 text-2xl text-primary'>
+            <ArrowLeft
+              className='cursor-pointer'
+              onClick={() => router.back()}
+            />
+            {searchParams?.name} Users
+          </h1>
+        </div>
+      )}
+      {data?.premiumAds?.ads.length > 0 && (
+        <div className='gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-6'>
+          {data &&
+            data?.premiumAds?.ads?.map(item => {
+              return (
+                <div
+                  key={item?.id}
+                  className=''
+                  onClick={() => {
+                    handleOpenModal()
+                    setSelectedData(item)
+                  }}
+                >
+                  <div className='border-2 border-white bg-[#d4d4d41a] border-opacity-15 rounded-xl cursor-pointer'>
+                    <Swiper
+                      breakpoints={{
+                        0: {
+                          slidesPerView: 1,
+                          spaceBetween: 20
+                        },
+                      }}
+              
+                      className='mySwiper'
+                    >
+                      {JSON.parse(item?.profile).map(it => {
+                        return (
+                          <SwiperSlide key={it} className='relative'>
+                            <ImageDisplay imageUrl={it} xs='200px' />
+                          </SwiperSlide>
+                        )
+                      })}
+                    </Swiper>
+                    <div className='py-4 p-2'>
+                      <h1 className='text-white'>{item?.title}</h1>
+                      <h1 className='mt-1 text-[12px] text-ellipsis text-white whitespace-nowrap overflow-hidden'>
+                        {item?.description}
+                      </h1>
+
+                      <div className='flex gap-2 mt-2'>
+                        <div className='flex items-center gap-2 bg-[#d4d4d41a] mt-2 px-2 py-1 rounded-xl text-[12px] text-white'>
+                          <Blend size={15} /> {item?.nationality}
+                        </div>
+                        <div className='flex items-center gap-1 bg-[#d4d4d41a] mt-2 px-2 py-1 rounded-xl text-[12px] text-white'>
+                          <User size={15} /> {item?.age}
+                        </div>
+                      </div>
+                      <div className='bg-[#d4d4d41a] mt-2 rounded-xl'>
+                        <h1 className='px-3 py-2 text-white'>
+                          ₹ {item?.pricePerHour} Per hour
+                        </h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+        </div>
+      )}
+      {open && (
+        <GlobalSearchModal
+          open={open}
+          close={() => setOpen(false)}
+          searchParams={searchParams}
+        />
+      )}
+      {isAddOpen && (
+        <AdsDetailsModal
+          open={isAddOpen}
+          close={handleCloseModal}
+          selectedData={selectedData}
+        />
+      )}
     </Container>
   )
 }
